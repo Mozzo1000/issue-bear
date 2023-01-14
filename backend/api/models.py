69e8f5b3e3ce46747978173e5a04408cf203d31e
Andreas Backström <mozzo242@gmail.com>
2023-01-14 19:26:04 +0100

    Add user table and auth. Closes #2

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,48 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma = Marshmallow()
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String, default="user")
+    status = db.Column(db.String, default="active")
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    @staticmethod
+    def generate_hash(password):
+        return generate_password_hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return check_password_hash(hash, password)
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        fields = ("id", "name", "email", "role", "status")
+
+
+class RevokedTokenModel(db.Model):
+    __tablename__ = 'revoked_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(120))
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def is_jti_blacklisted(cls, jti):
+        query = cls.query.filter_by(jti=jti).first()
+        return bool(query)

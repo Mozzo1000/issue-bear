8c3f24f69cb20a4f09e4b51cc4428ab6cbea03ce
Andreas Backström <mozzo242@gmail.com>
2023-01-17 19:39:25 +0100

    Add database env fallback

--- backend/api/config.py
+++ backend/api/config.py
@@ -6,5 +6,5 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://admin:password@localhost/issue-bear")
     JWT_ACCESS_TOKEN_EXPIRES = False # for development purposes

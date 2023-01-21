2f7f56edbc8cda16ac3c040ae30bfad2039b4c04
Andreas Backström <mozzo242@gmail.com>
2023-01-21 17:29:06 +0100

    Register auth and project routes

--- backend/api/app.py
+++ backend/api/app.py
@@ -4,6 +4,8 @@ from flask_jwt_extended import JWTManager
 from api.models import db, ma
 from api.config import DevConfig, ProdConfig
 import os
+from api.routes.auth import auth_endpoint
+from api.routes.projects import projects_endpoint
 
 app = Flask(__name__)
 if os.environ.get("FLASK_DEBUG") == "1":
@@ -14,6 +16,9 @@ db.init_app(app)
 ma.init_app(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
+
+app.register_blueprint(auth_endpoint)
+app.register_blueprint(projects_endpoint)
 
 @app.route("/")
 def index():

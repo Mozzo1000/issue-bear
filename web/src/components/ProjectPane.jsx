06cce7e2a7d7b1494f20bb430e539e0218826464
Andreas Backström <mozzo242@gmail.com>
2023-01-28 21:26:56 +0100

    Add generate new token button

--- web/src/components/ProjectPane.jsx
+++ web/src/components/ProjectPane.jsx
@@ -1,18 +1,47 @@
 import { Dropdown, Modal, Label, TextInput, Button } from 'flowbite-react'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import ProjectsService from '../services/projects.service';
 import { useToast } from '../useToast';
 
 function ProjectPane(props) {
     const [openTokenModal, setOpenTokenModal] = useState(false);
     const [copyButtonText, setCopyButtonText] = useState("Copy")
+    const [tokenText, setTokenText] = useState();
+
     const toast = useToast(4000);
 
     const toggleTokenModal = () => {
         setOpenTokenModal(!openTokenModal)
     };
 
+    const onClickGenerateToken = () => {
+        ProjectsService.generateToken(props.project.id).then(
+            response => {
+                toast("success", response.data.message);
+                setTokenText(response.data.token);
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                toggleTokenModal();
+                toast("error", resMessage);
+            }
+        )
+    };
+
+    useEffect(() => {
+        if (props.project) {
+            setTokenText(props.project.token)
+        }
+    }, [props.project])
+
+
     const copyTokenToClipboard = () => {
-        navigator.clipboard.writeText(props.project.token);
+        navigator.clipboard.writeText(tokenText);
         toast("success", "Token copied to clipboard!")
         setCopyButtonText("Copied!")
         setTimeout(() => {
@@ -43,11 +72,11 @@ function ProjectPane(props) {
                             </div>
                             <div className="flex flex-row space-x-4">
 
-                                <TextInput className="mb-2 w-full" id="token" type="text" disabled value={props.project.token} />
+                                <TextInput className="mb-2 w-full" id="token" type="text" disabled value={tokenText} />
                                 <Button disabled={copyButtonText == "Copied!"} onClick={copyTokenToClipboard}>{copyButtonText}</Button>
                             </div>
 
-                            <Button color="failure">Generate new token</Button>
+                            <Button color="failure" onClick={onClickGenerateToken}>Generate new token</Button>
                         </Modal.Body>
                     </Modal>
                 </>

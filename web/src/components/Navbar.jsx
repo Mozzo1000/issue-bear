7a3445b7b018615f4003894a2d85e9dc6c500b85
Andreas Backström <mozzo242@gmail.com>
2023-01-28 16:34:50 +0100

    Add avatar to navbar

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import { Navbar } from 'flowbite-react'
+import { Avatar, Dropdown, Navbar } from 'flowbite-react'
 import React from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Logo from "../assets/logo.svg";
+import AuthService from '../services/auth.service';
 
 function NavigationMenu() {
+    const currentUser = AuthService.getCurrentUser()
     const withouSidebarRoutes = ["/login", "/register"];
+    const navigate = useNavigate()
 
     const { pathname } = useLocation();
     if (withouSidebarRoutes.some((item) => pathname.includes(item)))
@@ -18,6 +21,26 @@ function NavigationMenu() {
                     Issue Bear
                 </span>
             </Navbar.Brand>
+            <div className="flex items-center md:order-2">
+                <Dropdown arrowIcon={false} inline={true} label={<Avatar alt="User settings" rounded={true} />}>
+                    <Dropdown.Header>
+                        <span className="block text-sm">
+                            {currentUser && currentUser["name"]}
+                        </span>
+                        <span className="block truncate text-sm font-medium">
+                            {currentUser && currentUser["email"]}
+                        </span>
+                    </Dropdown.Header>
+                    <Dropdown.Item>
+                        Settings
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item className="text-red-600" onClick={() => (AuthService.logout(), navigate(0))}>
+                        Sign out
+                    </Dropdown.Item>
+                </Dropdown>
+            </div>
+
             <Navbar.Toggle />
             <Navbar.Collapse>
                 <Navbar.Link href="#">

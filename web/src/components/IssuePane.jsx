fbf9be1987be88c20acfdb2de5ed689ccef4e48d
Andreas Backström <mozzo242@gmail.com>
2023-01-28 17:23:07 +0100

    Add styling to issue pane

--- web/src/components/IssuePane.jsx
+++ web/src/components/IssuePane.jsx
@@ -1,4 +1,4 @@
-import { Card, Button, Avatar } from 'flowbite-react';
+import { Card, Button, Avatar, Badge, Spinner } from 'flowbite-react';
 import React, { useState, useEffect } from 'react'
 import projectsService from '../services/projects.service';
 import { useToast } from '../useToast';
@@ -28,24 +28,40 @@ function IssuePane(props) {
         <div>
             <h1 className="text-lg font-bold pb-2">Issues</h1>
             {content &&
-                <div className="flex flex-col space-y-10">
-                    {content.issues.map(item => {
-                        return (
-                            <Card>
-                                <div className="flex flex-col">
-                                    <Avatar rounded />
-                                    <p>{item.description}</p>
-                                    <p>{item.email}</p>
-                                    <p>{item.tag}</p>
-                                </div>
-                                <div className="flex flex-wrap space-x-4">
-                                    <Button className="basis-1/2">Reply</Button>
-                                    <Button color="failure" className="basis-1/4">Delete</Button>
-                                </div>
-                            </Card>
-                        )
-                    })}
-                </div>
+                <>
+                    <div className="flex flex-col space-y-10">
+                        {content.issues.map(item => {
+                            return (
+                                <Card>
+                                    <div className="flex flex-col space-y-2">
+                                        <div className="flex flex-wrap">
+                                            <Badge>{item.tag}</Badge>
+                                        </div>
+                                        <p>{item.description}</p>
+                                        {item.email &&
+                                            <div>
+                                                <div className="flex flex-col pb-3">
+                                                    <dt className="mb-1 text-gray-500 text-lg">User</dt>
+                                                    <dd className="flex flex-wrap text-lg font-semibold">
+                                                        <Avatar rounded>
+                                                            <p>{item.email}</p>
+                                                        </Avatar></dd>
+                                                </div>
+                                            </div>
+                                        }
+                                    </div>
+                                    <div className="flex flex-wrap space-x-4">
+                                        <Button className="basis-1/2" disabled={!item.email}>Reply</Button>
+                                        <Button color="failure" className="basis-1/4">Delete</Button>
+                                    </div>
+                                </Card>
+                            )
+                        })}
+                    </div>
+                    {content.issues.length <= 0 &&
+                        <p>No issues found</p>
+                    }
+                </>
             }
         </div >
     )

0927c749bd601e1a523223710b102282a3879385
Andreas Backström <mozzo242@gmail.com>
2023-02-18 18:23:26 +0100

    Add mdx and tailwind/typography dependencies

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [{ enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) }, react()],
   server: {
     proxy: {
       '/v1': 'http://localhost:5000',

aa3cb1bfefa72a69ac3ad1381dd1565d3fffa1b0
Andreas Backström <mozzo242@gmail.com>
2023-07-03 20:56:10 +0200

    Add SVGR

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [{ enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) }, react()],
+  plugins: [svgr(), { enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) }, react()],
   server: {
     proxy: {
       '/v1': 'http://localhost:5000',

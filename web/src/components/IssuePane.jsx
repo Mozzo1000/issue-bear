f94c7e1443f716b4d034e9580332d7bcd3fc2f9a
Andreas Backström <mozzo242@gmail.com>
2023-07-04 15:16:20 +0200

    Change issue pane to only show active issues and make archvie button work

--- web/src/components/IssuePane.jsx
+++ web/src/components/IssuePane.jsx
@@ -1,6 +1,7 @@
 import { Card, Button, Avatar, Badge, Spinner, Tooltip } from 'flowbite-react';
 import React, { useState, useEffect } from 'react'
 import projectsService from '../services/projects.service';
+import issuesService from '../services/issues.service';
 import { useToast } from '../useToast';
 import TokenInfoCTA from './TokenInfoCTA';
 import { HiReply, HiArchive } from 'react-icons/hi';
@@ -9,7 +10,7 @@ function IssuePane(props) {
     const [content, setContent] = useState();
     const toast = useToast(4000);
 
-    useEffect(() => {
+    const getIssues = () => {
         setContent("");
         projectsService.getIssues(props.id).then(
             response => {
@@ -25,7 +26,29 @@ function IssuePane(props) {
                 toast("error", resMessage);
             }
         )
+    };
+
+    useEffect(() => {
+        getIssues();
     }, [props.id])
+
+    const archiveIssue = (id) => {
+        issuesService.archive(id).then(
+            response => {
+                toast("success", response.data.message)
+                getIssues();
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                toast("error", resMessage);
+            }
+        )
+    }
 
     const renderTagBadge = (value) => {
         let color = "info"
@@ -51,6 +74,7 @@ function IssuePane(props) {
                     <div className="flex flex-col space-y-10">
                         {content.issues.map(item => {
                             return (
+                                !item.archived &&
                                 <Card key={item.id}>
                                     <div className="flex flex-col space-y-2">
                                         <div className="flex justify-between">
@@ -76,7 +100,7 @@ function IssuePane(props) {
                                         <Tooltip content={item.email ? ("Reply to the user") : ("This user has not provided any email")}>
                                             <Button className="basis-1/4" disabled={!item.email} onClick={(e) => onClickEmail(e, item.description, item.email)}><HiReply className="mr-2"/>Reply</Button>
                                         </Tooltip>
-                                        <Button color="gray"><HiArchive className="mr-2"/>Archive</Button>
+                                        <Button color="gray" onClick={() => archiveIssue(item.id)}><HiArchive className="mr-2"/>Archive</Button>
                                     </div>
                                 </Card>
                             )

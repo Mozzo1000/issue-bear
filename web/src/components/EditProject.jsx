95870741b08498fb01352542de92eaa96028c4b3
Andreas Backström <mozzo242@gmail.com>
2023-07-04 17:53:28 +0200

    Add confirmation before project deletion

--- web/src/components/EditProject.jsx
+++ web/src/components/EditProject.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Modal, TextInput, Label, Button, Dropdown } from 'flowbite-react'
+import { Modal, TextInput, Label, Button, Dropdown, Alert } from 'flowbite-react'
 import ProjectsService from '../services/projects.service';
 import { useToast } from '../useToast';
+import { HiTrash, HiInformationCircle  } from 'react-icons/hi';
 
 function EditProject(props) {
     const [name, setName] = useState();
     const [url, setUrl] = useState();
     const [openModal, setOpenModal] = useState(false);
+    const [openConfirmationModal, setOpenConfirmationModal] = useState(false);
     const toast = useToast(8000);
 
     const toggleModal = () => {
@@ -45,6 +47,7 @@ function EditProject(props) {
     const removeProject = () => {
         ProjectsService.remove(props.project.id).then(
             response => {
+                setOpenConfirmationModal(false);
                 toggleModal();
                 console.log(response.data.message)
                 toast("success", response.data.message + ". Refresh the page for changes to take effect.");
@@ -86,7 +89,7 @@ function EditProject(props) {
                             <TextInput id="url" type="text" placeholder="https://google.com" required={false} value={url} onChange={e => setUrl(e.target.value)} />
                         </div>
                         <div>
-                            <Button onClick={removeProject} color="failure">Remove project</Button>
+                            <Button onClick={() => setOpenConfirmationModal(true)} color="failure">Remove project</Button>
                         </div>
                         <div className="flex flex-row space-x-4">
                             <Button type="submit">Save</Button>
@@ -94,6 +97,20 @@ function EditProject(props) {
                         </div>
                     </form>
                 </Modal.Body>
+            </Modal>
+
+            <Modal show={openConfirmationModal} size="md" onClose={() => setOpenConfirmationModal(false)} root={document.body}>
+                <Modal.Body className="prose lg:prose-lg flex flex-col items-center">
+                    <HiTrash size="3.5em"/>
+                    Are you sure you want to delete this project?
+                    <Alert color="failure" icon={HiInformationCircle}>
+                        This action cannot be <span className="font-medium">REVERTED!</span>
+                    </Alert>
+                </Modal.Body>
+                <Modal.Footer className="flex justify-center">
+                    <Button color="failure" onClick={removeProject}>Yes</Button>
+                    <Button color="light" onClick={() => setOpenConfirmationModal(false)}>No</Button>
+                </Modal.Footer>
             </Modal>
         </>
     )

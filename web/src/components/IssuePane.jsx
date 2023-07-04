7fbeede55edd39de34de62dcad8035f1dc9d2994
Andreas Backström <mozzo242@gmail.com>
2023-07-04 15:31:35 +0200

    Make use of issue card component and show archived issues in an accordion

--- web/src/components/IssuePane.jsx
+++ web/src/components/IssuePane.jsx
@@ -1,10 +1,11 @@
-import { Card, Button, Avatar, Badge, Spinner, Tooltip } from 'flowbite-react';
+import { Card, Button, Avatar, Badge, Spinner, Tooltip, Accordion } from 'flowbite-react';
 import React, { useState, useEffect } from 'react'
 import projectsService from '../services/projects.service';
 import issuesService from '../services/issues.service';
 import { useToast } from '../useToast';
 import TokenInfoCTA from './TokenInfoCTA';
 import { HiReply, HiArchive } from 'react-icons/hi';
+import IssueCard from './IssueCard';
 
 function IssuePane(props) {
     const [content, setContent] = useState();
@@ -50,21 +51,7 @@ function IssuePane(props) {
         )
     }
 
-    const renderTagBadge = (value) => {
-        let color = "info"
-        if (value == "bug") {
-            color = "failure"
-        } else if (value == "question") {
-            color = "warning"
-        }
-        return <Badge color={color}>{value}</Badge>
-    }
-
-    const onClickEmail = (event, desc, address) => {
-        var subject = "[Issue Bear] - Feedback " + desc.substring(0, 20) + "..."
-        var body = "%0D%0AOriginal feedback:%0D%0A" + desc;
-        window.location.href = "mailto:" + address + "?subject=" + subject + "&body=" + body;
-    }
+    
 
     return (
         <div>
@@ -75,42 +62,31 @@ function IssuePane(props) {
                         {content.issues.map(item => {
                             return (
                                 !item.archived &&
-                                <Card key={item.id}>
-                                    <div className="flex flex-col space-y-2">
-                                        <div className="flex justify-between">
-                                            {renderTagBadge(item.tag)}
-                                            <p className="font-semibold text-gray-800 text-xs">
-                                                {new Date(item.created_at).toLocaleDateString("en", {year: "numeric", day: "2-digit", month: "long"})}
-                                            </p>
-                                        </div>
-                                        <p>{item.description}</p>
-                                        {item.email &&
-                                            <div>
-                                                <div className="flex flex-col pb-3">
-                                                    <dt className="mb-1 text-gray-500 text-lg">User</dt>
-                                                    <dd className="flex flex-wrap text-lg font-semibold">
-                                                        <Avatar rounded>
-                                                            <p>{item.email}</p>
-                                                        </Avatar></dd>
-                                                </div>
-                                            </div>
-                                        }
-                                    </div>
-                                    <div className="flex flex-row justify-between">
-                                        <Tooltip content={item.email ? ("Reply to the user") : ("This user has not provided any email")}>
-                                            <Button className="basis-1/4" disabled={!item.email} onClick={(e) => onClickEmail(e, item.description, item.email)}><HiReply className="mr-2"/>Reply</Button>
-                                        </Tooltip>
-                                        <Button color="gray" onClick={() => archiveIssue(item.id)}><HiArchive className="mr-2"/>Archive</Button>
-                                    </div>
-                                </Card>
+                                <IssueCard item={item} onArchive={() => archiveIssue(item.id)} />
                             )
                         })}
-                    </div>
+                    
                     {content.issues.length <= 0 &&
                         <>
                             <TokenInfoCTA id={props.id} />
                         </>
                     }
+                    <Accordion collapseAll>
+                        <Accordion.Panel>
+                            <Accordion.Title>
+                                Archived issues
+                            </Accordion.Title>
+                            <Accordion.Content>
+                            {content.issues.map(item => {
+                                return (
+                                    item.archived &&
+                                    <IssueCard item={item} onArchive={() => archiveIssue(item.id)} disableArchive={true} />
+                                )
+                            })}
+                            </Accordion.Content>
+                        </Accordion.Panel>
+                    </Accordion>
+                    </div>
                 </>
             ): (
                 <Spinner/>
